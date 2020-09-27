832247de46ec134e577e59be30832c033b8b8167
Alex Dong <adong@ualberta.ca>
2020-09-26 20:19:01 -0600

    add initial typing

---
+++ src/types/information.ts
@@ -0,0 +1,12 @@
+export interface LinkDetails {
+  id: string;
+  type: string;
+  url: string;
+}
+
+export interface WorkDetails {
+  id: string;
+  company: string;
+  position: string;
+  description: string;
+}
