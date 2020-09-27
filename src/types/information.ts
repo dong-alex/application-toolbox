c9a0da149a7ab6a855609d9effe78cbeef4950c0
Alex Dong <adong@ualberta.ca>
2020-09-27 01:16:21 -0600

    seperate firebase entries with new submissions

--- src/types/information.ts
+++ src/types/information.ts
@@ -1,12 +1,22 @@
-export interface LinkDetails {
+// ids are optional until it is registerd into firebase
+export interface LinkDetails extends LinkBase {
   id: string;
+}
+
+export interface WorkDetails extends WorkBase {
+  id: string;
+}
+
+export interface LinkBase {
   type: string;
   url: string;
 }
 
-export interface WorkDetails {
-  id: string;
+export interface WorkBase {
   company: string;
   position: string;
   description: string;
+  startDate: Date;
+  endDate: Date | null; // if current - then endDate = null
+  current: boolean;
 }
